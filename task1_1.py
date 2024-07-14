4a6d597752867ecf8dd4c8acefad93355d220077
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2024-07-14 21:15:01 +0300

    Delete task1_1.py

--- task1_1.py
+++
@@ -1,8 +0,0 @@
-def count1(i_list):
-    count = i_list.count(4)
-    return count
-
-E_list = [2 , 4 , 3 , 4 , 8 , 4, 4 ,4]
-num_fours = count1(E_list)
-print("The number 4 appears" , num_fours," times in the list.")
-
