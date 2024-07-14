7baf29dbae31a80deac081441f4190083534c5a2
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2024-07-14 21:15:11 +0300

    Delete task1_2.py

--- task1_2.py
+++
@@ -1,6 +0,0 @@
-print("PLZ Enter the Letter")
-Letter = input("Please enter a Letter: ")
-if Letter in {'A' ,'a','E','e','I','i','O','o','U','u'}:
-       print("IT IS VOWEL LETTER ")
-else:
-       print("IT IS NOT VOWEL")
