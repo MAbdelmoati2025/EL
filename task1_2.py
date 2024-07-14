4f3d34b0cacc81f878a00baffd0f9ed83b0b2067
MAbdelmoati2025 <142133829+MAbdelmoati2025@users.noreply.github.com>
2024-07-14 21:13:47 +0300

    Add files via upload

---
+++ task1_2.py
@@ -0,0 +1,6 @@
+print("PLZ Enter the Letter")
+Letter = input("Please enter a Letter: ")
+if Letter in {'A' ,'a','E','e','I','i','O','o','U','u'}:
+       print("IT IS VOWEL LETTER ")
+else:
+       print("IT IS NOT VOWEL")
